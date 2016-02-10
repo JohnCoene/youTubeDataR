fc33c6710436645d7068a3ef09f991fa4728c61e
CMAnalytics <john.coene@conew.com>
2016-02-10 16:01:43 +0800

    example youOAuth fixed

--- R/find_params.R
+++ R/find_params.R
@@ -32,7 +32,7 @@
 #' @examples 
 #' \dontrun{
 #' # Authenticate
-#' token <- youOauth(client.id = "something.apps.googleusercontent.com",
+#' token <- youOAuth(client.id = "something.apps.googleusercontent.com",
 #'                   client.secret = "XxxXX1XxXxXxxx1xxx1xxXXX")
 #' 
 #' # find valid values for video.dimension
